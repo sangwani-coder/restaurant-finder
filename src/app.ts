67ce7fee83cd90c28ef46a4f1cdab2677a487068
zyambo <mightypz2@gmail.com>
2025-12-05 12:16:23 +0200

    feat: Add request logging

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { checkStatus, findRestaurants } from './controllers/searchController';
 
+var morgan = require('morgan');
 
 const app = express();
 
 app.use(express.json());
+app.use(morgan('combined'))
 
 //  Routes
 app.use("/", checkStatus);
