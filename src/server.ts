585dd9f70381dda760e974150cb8cdea828a1b93
zyambo <mightypz2@gmail.com>
2025-12-08 13:09:16 +0200

    fix: remove LAN access ipaddres

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,6 @@ import { initializeConfig } from './config/config';
 dotenv.config();
 const config = initializeConfig()
 
-app.listen(config.port,'192.168.8.100', () => {
+app.listen(config.port, () => {
     console.log(`Server is running on port ${config.port}`);
 })
